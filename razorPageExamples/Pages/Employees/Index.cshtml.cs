e4d93d34309dae024ebdabce558633180c82bd8a
HTarık <244035008@ogr.cbu.edu.tr>
2025-04-18 21:02:50 +0300

    feat: Add Employees Page and Model

---
+++ razorPageExamples/Pages/Employees/Index.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace razorPageExamples.Pages.Employees;
+
+public class IndexModel : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
